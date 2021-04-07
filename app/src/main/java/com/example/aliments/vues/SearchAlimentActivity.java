6bdd61817300e201bbf6355e73fa0939d8de1e3b
bouzoubaafahd <fahde.bouzoubaa@etu.univ-cotedazur.fr>
2021-04-07 20:07:04 +0200

    test horizontal

--- app/src/main/java/com/example/aliments/vues/SearchAlimentActivity.java
+++ app/src/main/java/com/example/aliments/vues/SearchAlimentActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
