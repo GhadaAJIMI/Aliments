efeeb5f822d01965d33dba89d30a265ea6c5271f
Floriane Paris <floriane99.paris@free.fr>
2021-04-28 11:51:01 +0200

    Ajout classes pour photo

---
+++ app/src/main/java/com/example/aliments/modeles/IPictureActivity.java
@@ -0,0 +1,5 @@
+package com.example.aliments.modeles;
+
+public interface IPictureActivity {
+    int REQUEST_CAMERA = 100;
+}
---
+++ app/src/main/java/com/example/aliments/vues/AjoutAlimentActivity.java
@@ -0,0 +1,16 @@
+package com.example.aliments.vues;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aliments.R;
+
+public class AjoutAlimentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_aliment);
+    }
+}
