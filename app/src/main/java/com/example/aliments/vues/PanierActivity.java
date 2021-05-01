63e287436e1466c29a6374075b77d81da73b2950
Ferdaouss Chakroun <ferdaouss.chakroun@etu.univ-cotedazur.fr>
2021-05-01 21:07:21 +0200

    afficher la liste des aliments

--- app/src/main/java/com/example/aliments/vues/PanierActivity.java
+++ app/src/main/java/com/example/aliments/vues/PanierActivity.java
@@ -19,6 +19,7 @@ import com.example.aliments.controleurs.MagasinControler;
 import com.example.aliments.controleurs.UserControler;
 import com.example.aliments.modeles.Aliment;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
@@ -51,15 +52,15 @@ public class PanierActivity extends AppCompatActivity {
 
         addEvent = findViewById(R.id.agenda);
         addEvent.setOnClickListener(new View.OnClickListener() {
-            public String convertWithIteration(HashMap<Aliment, Double> map) {
-                map = UserControler.get(0).getBasket().getListeProduit2();
-                StringBuilder mapAsString = new StringBuilder("{");
-                for (Aliment key : map.keySet()) {
-                    mapAsString.append(key + "=" + map.get(key) + ", ");
-                }
-                mapAsString.delete(mapAsString.length()-2, mapAsString.length()).append("}");
-                return mapAsString.toString();
+
+            private String convertWithIteration(HashMap<Aliment, Double> map) {
+                String res = "";
+
+                ArrayList<Aliment> arrayList = new ArrayList(map.keySet());
+                for(Aliment item : arrayList) res += item.getName()+"\n";
+                return res;
             }
+
 
             @Override
             public void onClick(View v) {
@@ -69,9 +70,9 @@ public class PanierActivity extends AppCompatActivity {
 
                 HashMap<Aliment, Double> listBasket = UserControler.get(0).getBasket().getListeProduit2();
 
-                intent.putExtra(CalendarContract.Events.DESCRIPTION, "Votre panier contient: " + convertWithIteration(listBasket) );
+                intent.putExtra(CalendarContract.Events.DESCRIPTION, "Mon panier contient: \n" + convertWithIteration(listBasket) );
                 intent.putExtra(CalendarContract.Events.DTSTART, Calendar.getInstance().getTimeInMillis());
-                intent.putExtra(CalendarContract.Events.DTEND, Calendar.getInstance().getTimeInMillis() +60*60*1000);
+                intent.putExtra(CalendarContract.Events.DTEND, Calendar.getInstance().getTimeInMillis() +48*60*60*1000);
                 intent.putExtra(CalendarContract.Events.ALL_DAY, true);
                 startActivity(intent);
 
