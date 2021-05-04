9f386bc9bdb3a9395a4344723c76418e311a9048
Floriane Paris <floriane99.paris@free.fr>
2021-05-04 22:53:58 +0200

    Ajout d'autant de positions qu'on veut

--- app/src/main/java/com/example/aliments/vues/MapActivity.java
+++ app/src/main/java/com/example/aliments/vues/MapActivity.java
@@ -1,23 +1,13 @@
  package com.example.aliments.vues;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.preference.PreferenceManager;
 
-
-import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.example.aliments.R;
 
@@ -34,17 +24,16 @@ import org.osmdroid.views.overlay.OverlayItem;
 
 import java.util.ArrayList;
 
+//43.65000, 7.01000
 public class MapActivity extends AppCompatActivity {
     private MapView  map;
 
-    private static final int REQUEST_LOCATION = 1;
-    Button btnGetLocation;
-    LocationManager locationManager;
-    OverlayItem actual;
+    int nbMagasins;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        nbMagasins = 1;
         Configuration.getInstance().load(getApplicationContext(),
                 PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         setContentView(R.layout.activity_map);
@@ -58,10 +47,8 @@ public class MapActivity extends AppCompatActivity {
         mapController.setZoom(18.0);                // Définir zoom de départ
 
         ArrayList<OverlayItem> magasins = new ArrayList<>();
-        OverlayItem shop1 = new OverlayItem("shop1", "Magasin de légumes", new GeoPoint(43.65020, 7.00517));
-        magasins.add(shop1);
-        OverlayItem shop2 = new OverlayItem("shop2", "Boucherie", new GeoPoint(43.64950, 7.00515));
-        magasins.add(shop2);
+        createmarker(43.65020, 7.00517);
+        createmarker(43.64950, 7.00515);
 
         ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(),
                 magasins, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
@@ -78,19 +65,18 @@ public class MapActivity extends AppCompatActivity {
         mOverlay.setFocusItemsOnTap(true);
         map.getOverlays().add(mOverlay);
 
-        ActivityCompat.requestPermissions( this,
-                new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        btnGetLocation = findViewById(R.id.btnGetLocation);
-        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+        TextView addressLat = (TextView) findViewById(R.id.addressLatitude);
+        TextView addressLong = (TextView) findViewById(R.id.addressLongitude);
+
+        Button addressButton = (Button) findViewById(R.id.btnGetLocation);
+        addressButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                    OnGPS();
-                } else {
-                    getLocation();
-                    magasins.add(actual);
-                }
+            public void onClick(View arg0) {
+
+                Double lat =  Double.valueOf(addressLat.getText().toString());
+                Double longi = Double.valueOf(addressLong.getText().toString());
+
+                createmarker(lat, longi);
             }
         });
 
@@ -114,49 +100,15 @@ public class MapActivity extends AppCompatActivity {
         map.onResume();
     }
 
-    private void OnGPS() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-            }
-        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        final AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                MapActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                MapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        } else {
-            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (locationGPS != null) {
-                double lat = locationGPS.getLatitude();
-                double longi = locationGPS.getLongitude();
-
-                Toast.makeText(this, "long:"+longi+" lat:"+lat, Toast.LENGTH_SHORT).show();
-                createmarker();
-            } else {
-                Toast.makeText(this, "Unable to find location.", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    public void createmarker(){
+    public void createmarker(Double latitude, Double longitude){
         if(map == null) {
             return;
         }
         Marker my_marker = new Marker(map);
-        my_marker.setPosition(new GeoPoint(43.64750, 7.01515));
+        my_marker.setPosition(new GeoPoint(latitude, longitude));
         my_marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        my_marker.setTitle("new Shop");
+        my_marker.setTitle("Shop "+nbMagasins);
+        nbMagasins = nbMagasins+1;
         my_marker.setPanToView(true);
         map.getOverlays().add(my_marker);
         map.invalidate();
